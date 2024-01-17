525ed2343c84d23c18a24be51f6ff5e5a5cd0caa
ArtemiyGranat <artemiigranat@gmail.com>
2024-01-17 13:21:22 +0400

    logging and cors middleware

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,19 @@
+import logging
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+logger = logging.getLogger("app")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[], # TODO
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
+@app.get("/", summary="Hello, world")
 def hello():
     return "Hello, world!"
