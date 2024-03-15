e167c16b91f37207da5d097bd961d0d0ded539db
Artemiy Granat <artemiigranat@gmail.com>
2024-03-15 21:12:04 +0400

    add init.sql file for postgres & pgvector

---
+++ db/init.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE isat_cv_db;
+\c isat_cv_db
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "vector";
+
+CREATE TABLE images (
+    id uuid DEFAULT uuid_generate_v4(),
+    url varchar(512) UNIQUE NOT NULL,
+    hash varchar(128) UNIQUE NOT NULL,
+    hsv vector(3),
+    lab vector(3),
+    image_embeddings vector(2000),
+    text_embeddings vector(2000),
+    is_processed boolean NOT NULL,
+
+    PRIMARY KEY(id)
+)
